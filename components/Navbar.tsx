67fddd73f2e1503e748031a2c7947be4fd51c2df
Delon Pratama <delonpratama1009@gmail.com>
2026-02-19 21:46:18 +0700

    feat: convert logo to component with tailwind support for dynamic colors and changed nav text colors

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 
+import Logo from "./ui/Logo";
 import { navItems } from "@/constants/navigation";
 
 export default function Navbar() {
   return (
     <div className="flex flex-row py-7 justify-between">
       <Link href="/" className="flex gap-3">
-        <img src="/logo.svg" alt="Berita Kini" className="h-11 w-auto" />
+        <Logo className="text-primary" />
         <p className="flex items-center text-xl font-semibold">Berita Kini</p>
       </Link>
-      <ul className="flex gap-8 text-body-medium items-center">
+      <ul className="flex gap-8 text-body-medium items-center text-primary-text">
         {navItems.map((item) => (
           <Link
             className="hover:text-primary transition-colors duration-300"
