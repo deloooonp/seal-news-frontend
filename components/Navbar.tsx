37d443d9921be3eaf050b4be03836b7552f7a2a8
Delon Pratama <delonpratama1009@gmail.com>
2026-02-19 21:19:29 +0700

    chore: removed text-white from logo classname

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -34,11 +34,7 @@ export default function Navbar() {
   return (
     <div className="flex flex-row py-7 justify-between">
       <Link href="/" className="flex gap-3">
-        <img
-          src="/logo.svg"
-          alt="Berita Kini"
-          className="h-11 w-auto text-white"
-        />
+        <img src="/logo.svg" alt="Berita Kini" className="h-11 w-auto" />
         <p className="flex items-center text-xl font-semibold">Berita Kini</p>
       </Link>
       <ul className="flex gap-8 text-body-medium items-center">
